e6f6bd91a8f76f9c725c3baffea906c3736ccbfa
Tony Barboza <ajb1047@wildcats.unh.edu>
2019-11-25 12:22:07 -0500

    Tony

---
+++ src/main/java/lucene/SynonymFinder.java
@@ -0,0 +1,55 @@
+package lucene;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import edu.smu.tspell.wordnet.*;
+
+
+public class SynonymFinder {
+	public static WordNetDatabase _database;
+	
+	public SynonymFinder() {
+		File f=new File("./src/main/java/data/WordNet-3.0/dict");
+        System.setProperty("wordnet.database.dir", f.toString());
+        //setting path for the WordNet Directory
+        WordNetDatabase database = WordNetDatabase.getFileInstance();
+        _database = database;
+        
+	}
+	public static void findSyn( String wordForm ) {
+		Synset[] synsets = _database.getSynsets(wordForm);
+
+        if (synsets.length > 0){
+           //ArrayList<String> al = new ArrayList<String>();
+           // add elements to al, including duplicates
+           HashSet<String> hs = new HashSet<String>();
+           for (int i = 0; i < synsets.length; i++){
+         	  if ( i == 3 ) {
+         		  break;
+         	  }
+              String[] wordForms = synsets[i].getWordForms();
+              for (int j = 0; j < wordForms.length; j++)
+              {
+             	 hs.add(wordForms[j]);
+              }
+           }
+           //System.out.println(hs.size());
+           Iterator<String> it = hs.iterator();
+           String tmp;
+           while( it.hasNext() ) {
+         	  tmp = it.next();
+         	  System.out.println(tmp);
+           }              
+        }
+        else
+        {
+     	   System.err.println("No synsets exist that contain the word form '" + wordForm + "'");
+        }
+	}
+	public static void main(String[] args) {
+
+	}
+}
